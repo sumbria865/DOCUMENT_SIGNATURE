cc952fea15d3b6c2678bff97296d3b46523f93d6
Sumbrai865 <devi.prinka23@spit.ac.in>
2026-02-21 22:29:56 +0530

    Deployed bac again

--- Frontend/src/utils/constants.ts
+++ Frontend/src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://back-sign-5trk.onrender.com/api'
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://bac-dep.onrender.com/api';
 
 export const ROUTES = {
   HOME: '/',
